3f92ee5e1adcf11e52df8e99bd4ee9eb3be61314
Paweł Sękara <pawel.sekara@codewise.com>
2016-08-12 14:42:14 +0200

    Deleted unused files

--- ReactiveCollection/Sources/Extensions/HelperExtensions.swift
+++ ReactiveCollection/Sources/Extensions/HelperExtensions.swift
@@ -10,6 +10,22 @@ import Foundation
 import ObjectiveC.runtime
 import UIKit
 
+
+public protocol DataReloadable: class {
+    static var optionalDataSourceSelectors: [String] { get }
+    
+    func reloadData()
+}
+
+extension UITableView: DataReloadable {
+    @nonobjc public static var optionalDataSourceSelectors: [String] = RACDataSourceProxy.optionalSelectorsFor(UITableViewDataSource)
+}
+
+extension UICollectionView: DataReloadable {
+    @nonobjc public static var optionalDataSourceSelectors: [String] = RACDataSourceProxy.optionalSelectorsFor(UICollectionViewDataSource)
+}
+
+
 extension NSObject {
     
     var methodsAndSelectors: (UnsafeMutablePointer<Method>, [String]) {
@@ -24,17 +40,28 @@ extension NSObject {
     
 }
 
-
-public protocol DataReloadable: class {
-    static var optionalDataSourceSelectors: [String] { get }
+extension RACDataSourceProxy {
     
-    func reloadData()
+    public class func displayWarningsIfNeeded(selfDataSource: AnyObject?, newDataSource: AnyObject?) {
+        if newDataSource is RACDataSourceProxy && !(selfDataSource is RACDataSourceProxy) && selfDataSource != nil {
+            print("Warning: Binding dataSource with RAC will override one that is already set.")
+            print("         Setting dataSource to nil before binding will silence this warning.")
+        } else if selfDataSource is RACDataSourceProxy && newDataSource != nil {
+            print("Warning: Setting dataSource will override RAC bindings.")
+            print("         If you would like to keep RAC bindings and use other dataSource methods, please use forwardDataSource property instead.")
+            print("         If you are sure you want to override RAC bindings, set dataSource to nil before setting new one.")
+        }
+    }
 }
 
-extension UITableView: DataReloadable {
-    @nonobjc public static var optionalDataSourceSelectors: [String] = RACDataSourceProxy.optionalSelectorsFor(UITableViewDataSource)
-}
-
-extension UICollectionView: DataReloadable {
-    @nonobjc public static var optionalDataSourceSelectors: [String] = RACDataSourceProxy.optionalSelectorsFor(UICollectionViewDataSource)
+extension RACDataSourceProxy {
+    class func optionalSelectorsFor(proto: Protocol) -> [String] {
+        var outCount: UInt32 = 0
+        let protocolMethods = protocol_copyMethodDescriptionList(proto, false, true, &outCount)
+        var protocolMethodStrings: [String] = []
+        for i in 0 ..< Int(outCount) {
+            protocolMethodStrings.append(String(protocolMethods[i].name))
+        }
+        return protocolMethodStrings
+    }
 }
--- ReactiveCollection/Sources/Protocols/RACDelegateProxy.swift
+++ ReactiveCollection/Sources/Protocols/RACDelegateProxy.swift
@@ -55,28 +55,3 @@ public class RACDataSourceProxy: NSObject, RACDataSourceProxyType {
     }
 }
 
-extension RACDataSourceProxy {
-    
-    public class func displayWarningsIfNeeded(selfDataSource: AnyObject?, newDataSource: AnyObject?) {
-        if newDataSource is RACDataSourceProxy && !(selfDataSource is RACDataSourceProxy) && selfDataSource != nil {
-            print("Warning: Binding dataSource with RAC will override one that is already set.")
-            print("         Setting dataSource to nil before binding will silence this warning.")
-        } else if selfDataSource is RACDataSourceProxy && newDataSource != nil {
-            print("Warning: Setting dataSource will override RAC bindings.")
-            print("         If you would like to keep RAC bindings and use other dataSource methods, please use forwardDataSource property instead.")
-            print("         If you are sure you want to override RAC bindings, set dataSource to nil before setting new one.")
-        }
-    }
-}
-
-extension RACDataSourceProxy {
-    class func optionalSelectorsFor(proto: Protocol) -> [String] {
-        var outCount: UInt32 = 0
-        let protocolMethods = protocol_copyMethodDescriptionList(proto, false, true, &outCount)
-        var protocolMethodStrings: [String] = []
-        for i in 0 ..< Int(outCount) {
-            protocolMethodStrings.append(String(protocolMethods[i].name))
-        }
-        return protocolMethodStrings
-    }
-}
