a60ec32ab56216832c2f975712bafde33c4f871b
Paweł Sękara <pawel.sekara@codewise.com>
2016-10-12 11:24:26 +0200

    Added watchos target

---
+++ ReactiveUIBindings watchOS/ReactiveUIBindings watchOS.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveUIBindings watchOS.h
+//  ReactiveUIBindings watchOS
+//
+//  Created by Paweł Sękara on 12.10.2016.
+//  Copyright © 2016 Codewise Sp. z o.o. Sp. K. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for ReactiveUIBindings watchOS.
+FOUNDATION_EXPORT double ReactiveUIBindings_watchOSVersionNumber;
+
+//! Project version string for ReactiveUIBindings watchOS.
+FOUNDATION_EXPORT const unsigned char ReactiveUIBindings_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveUIBindings_watchOS/PublicHeader.h>
+
+
